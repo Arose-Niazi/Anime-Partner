da8f80023bcddcd67c67c0489c8b62a11c13e859
123qwe456 <aeman098.fatima@gmail.com>
2020-11-19 18:23:29 +0500

    Pages

---
+++ js/list.js
@@ -0,0 +1,251 @@
+function list(){
+    document.getElementById("box").innerHTML = 
+    '<ul class="collection">'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                '<li class="collection-item">Death Note</li>'+
+                '<li class="collection-item">ullmetal Alchemist: Brotherhood.</li>'+
+                '<li class="collection-item">Naruto</li>'+
+                '<li class="collection-item">AlvAttack on Titan</li>'+
+                
+              '</ul>'
+}
+
+function grid(){
+    el1 = document.getElementById('box');
+
+    output = '<div class="row">'+
+        '<div class="col s3">'+
+        '<div class="card txtBox">'+
+                '<div class="card-image">'+
+                    '<img src="images/Dragon.png">'+
+                '</div>'+
+                '<div class="card-content">'+
+                    '<p>'+
+                        '<span class="margin">Name: Bla Bla</span><br>'+
+                        '<span>Ratings:'+  
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                        '</span><br>'+
+                        '<span>Genre: Bla Bla</span><br>'+
+                    '</p>'+
+                '</div>'+ 
+            '</div>'+
+        '</div>'+
+
+        '<div class="col s3">'+
+            '<div class="card txtBox">'+
+                '<div class="card-image">'+
+                    '<img src="images/Dragon.png">'+
+                '</div>'+
+                '<div class="card-content">'+
+                    '<p>'+
+                        '<span class="margin">Name: Bla Bla</span><br>'+
+                        '<span>Ratings:'+  
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                        '</span><br>'+
+                        '<span>Genre: Bla Bla</span><br>'+
+                    '</p>'+
+                '</div>'+ 
+            '</div>'+
+        '</div>'+
+
+        '<div class="col s3">'+
+            '<div class="card txtBox">'+
+                '<div class="card-image">'+
+                    '<img src="images/Dragon.png">'+
+                '</div>'+
+                '<div class="card-content">'+
+                    '<p>'+
+                        '<span class="margin">Name: Bla Bla</span><br>'+
+                        '<span>Ratings:'+  
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                        '</span><br>'+
+                        '<span>Genre: Bla Bla</span><br>'+
+                    '</p>'+
+                '</div>'+ 
+            '</div>'+
+        '</div>'+
+
+        '<div class="col s3">'+
+            '<div class="card txtBox">'+
+                '<div class="card-image">'+
+                    '<img src="images/Dragon.png">'+
+                '</div>'+
+                '<div class="card-content">'+
+                    '<p>'+
+                        '<span class="margin">Name: Bla Bla</span><br>'+
+                        '<span>Ratings:'+  
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                        '</span><br>'+
+                        '<span>Genre: Bla Bla</span><br>'+
+                    '</p>'+
+                '</div>'+ 
+            '</div>'+
+        '</div>'+
+        '</div>'; //initialize output string
+
+    //build output string
+    for(var i=0; i<=5; i++){
+        output +='<div class="row">'+
+        '<div class="col s3">'+
+        '<div class="card txtBox">'+
+                '<div class="card-image">'+
+                    '<img src="images/Dragon.png">'+
+                '</div>'+
+                '<div class="card-content">'+
+                    '<p>'+
+                        '<span class="margin">Name: Bla Bla</span><br>'+
+                        '<span>Ratings:'+  
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                        '</span><br>'+
+                        '<span>Genre: Bla Bla</span><br>'+
+                    '</p>'+
+                '</div>'+ 
+            '</div>'+
+        '</div>'+
+
+        '<div class="col s3">'+
+            '<div class="card txtBox">'+
+                '<div class="card-image">'+
+                    '<img src="images/Dragon.png">'+
+                '</div>'+
+                '<div class="card-content">'+
+                    '<p>'+
+                        '<span class="margin">Name: Bla Bla</span><br>'+
+                        '<span>Ratings:'+  
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                        '</span><br>'+
+                        '<span>Genre: Bla Bla</span><br>'+
+                    '</p>'+
+                '</div>'+ 
+            '</div>'+
+        '</div>'+
+
+        '<div class="col s3">'+
+            '<div class="card txtBox">'+
+                '<div class="card-image">'+
+                    '<img src="images/Dragon.png">'+
+                '</div>'+
+                '<div class="card-content">'+
+                    '<p>'+
+                        '<span class="margin">Name: Bla Bla</span><br>'+
+                        '<span>Ratings:'+  
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                        '</span><br>'+
+                        '<span>Genre: Bla Bla</span><br>'+
+                    '</p>'+
+                '</div>'+ 
+            '</div>'+
+        '</div>'+
+
+        '<div class="col s3">'+
+            '<div class="card txtBox">'+
+                '<div class="card-image">'+
+                    '<img src="images/Dragon.png">'+
+                '</div>'+
+                '<div class="card-content">'+
+                    '<p>'+
+                        '<span class="margin">Name: Bla Bla</span><br>'+
+                        '<span>Ratings:'+  
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                            '<span class="fa fa-star checked"></span>'+
+                        '</span><br>'+
+                        '<span>Genre: Bla Bla</span><br>'+
+                    '</p>'+
+                '</div>'+ 
+            '</div>'+
+        '</div>'+
+    '</div>';
+    }
+
+    el1.innerHTML = output; //output to DOM
+}
